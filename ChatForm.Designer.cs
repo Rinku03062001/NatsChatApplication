7b38aeccae8d747bef69106f79d8804b9f048ce2
Tapobanta <tapobanta43@gmail.com>
2025-09-10 11:34:47 +0530

    Add a new button (btnSendFile) to the Designer

--- ChatForm.Designer.cs
+++ ChatForm.Designer.cs
@@ -28,37 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChatForm));
             listBox1 = new ListBox();
             txtMessage = new TextBox();
             btnSend = new Button();
+            btnSendFile = new Button();
             SuspendLayout();
             // 
             // listBox1
             // 
             listBox1.FormattingEnabled = true;
-            listBox1.Location = new Point(12, 12);
+            listBox1.Location = new Point(6, 12);
             listBox1.Name = "listBox1";
             listBox1.Size = new Size(776, 344);
             listBox1.TabIndex = 0;
             // 
             // txtMessage
             // 
-            txtMessage.Location = new Point(7, 362);
+            txtMessage.Location = new Point(95, 362);
             txtMessage.Multiline = true;
             txtMessage.Name = "txtMessage";
-            txtMessage.Size = new Size(625, 75);
+            txtMessage.Size = new Size(564, 75);
             txtMessage.TabIndex = 1;
             // 
             // btnSend
             // 
             btnSend.BackColor = SystemColors.ControlLight;
-            btnSend.Location = new Point(640, 366);
+            btnSend.Location = new Point(665, 366);
             btnSend.Name = "btnSend";
-            btnSend.Size = new Size(147, 67);
+            btnSend.Size = new Size(122, 67);
             btnSend.TabIndex = 2;
             btnSend.Text = "Send\r\n";
             btnSend.UseVisualStyleBackColor = true;
             btnSend.Click += btnSend_Click;
+            // 
+            // btnSendFile
+            // 
+            btnSendFile.Location = new Point(6, 362);
+            btnSendFile.Name = "btnSendFile";
+            btnSendFile.Size = new Size(83, 65);
+            btnSendFile.TabIndex = 3;
+            btnSendFile.Text = "SendFile";
+            btnSendFile.UseVisualStyleBackColor = true;
+            btnSendFile.Click += btnSendFile_Click;
             // 
             // ChatForm
             // 
@@ -66,14 +78,16 @@
             AutoScaleMode = AutoScaleMode.Font;
             AutoSize = true;
             ClientSize = new Size(794, 440);
+            Controls.Add(btnSendFile);
             Controls.Add(btnSend);
             Controls.Add(txtMessage);
             Controls.Add(listBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             MaximizeBox = false;
             Name = "ChatForm";
             SizeGripStyle = SizeGripStyle.Hide;
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "ChatForm";
+            Text = "Synapse";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -83,6 +97,6 @@
         private ListBox listBox1;
         private TextBox txtMessage;
         private Button btnSend;
-        
+        private Button btnSendFile;
     }
 }

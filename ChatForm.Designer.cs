bda4b386c24914b3bb0e3830705afc8b77a19d06
Tapobanta <tapobanta43@gmail.com>
2025-09-10 12:07:42 +0530

    Added a FlowLayoutPanel instead of ListBox and Dock it as top, Add a panel and dock is a bottom and adjust the properties

--- ChatForm.Designer.cs
+++ ChatForm.Designer.cs
@@ -29,34 +29,28 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChatForm));
-            listBox1 = new ListBox();
             txtMessage = new TextBox();
             btnSend = new Button();
             btnSendFile = new Button();
+            showMessageLayoutPanel = new FlowLayoutPanel();
+            panel1 = new Panel();
+            panel1.SuspendLayout();
             SuspendLayout();
-            // 
-            // listBox1
-            // 
-            listBox1.FormattingEnabled = true;
-            listBox1.Location = new Point(6, 12);
-            listBox1.Name = "listBox1";
-            listBox1.Size = new Size(776, 344);
-            listBox1.TabIndex = 0;
             // 
             // txtMessage
             // 
-            txtMessage.Location = new Point(95, 362);
+            txtMessage.Location = new Point(100, 11);
             txtMessage.Multiline = true;
             txtMessage.Name = "txtMessage";
-            txtMessage.Size = new Size(564, 75);
+            txtMessage.Size = new Size(600, 67);
             txtMessage.TabIndex = 1;
             // 
             // btnSend
             // 
             btnSend.BackColor = SystemColors.ControlLight;
-            btnSend.Location = new Point(665, 366);
+            btnSend.Location = new Point(706, 11);
             btnSend.Name = "btnSend";
-            btnSend.Size = new Size(122, 67);
+            btnSend.Size = new Size(100, 67);
             btnSend.TabIndex = 2;
             btnSend.Text = "Send\r\n";
             btnSend.UseVisualStyleBackColor = true;
@@ -64,7 +58,7 @@
             // 
             // btnSendFile
             // 
-            btnSendFile.Location = new Point(6, 362);
+            btnSendFile.Location = new Point(9, 13);
             btnSendFile.Name = "btnSendFile";
             btnSendFile.Size = new Size(83, 65);
             btnSendFile.TabIndex = 3;
@@ -72,31 +66,49 @@
             btnSendFile.UseVisualStyleBackColor = true;
             btnSendFile.Click += btnSendFile_Click;
             // 
+            // showMessageLayoutPanel
+            // 
+            showMessageLayoutPanel.AutoScroll = true;
+            showMessageLayoutPanel.Dock = DockStyle.Top;
+            showMessageLayoutPanel.Location = new Point(0, 0);
+            showMessageLayoutPanel.Name = "showMessageLayoutPanel";
+            showMessageLayoutPanel.Size = new Size(814, 353);
+            showMessageLayoutPanel.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(btnSendFile);
+            panel1.Controls.Add(btnSend);
+            panel1.Controls.Add(txtMessage);
+            panel1.Location = new Point(3, 359);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(809, 86);
+            panel1.TabIndex = 4;
+            // 
             // ChatForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoSize = true;
-            ClientSize = new Size(794, 440);
-            Controls.Add(btnSendFile);
-            Controls.Add(btnSend);
-            Controls.Add(txtMessage);
-            Controls.Add(listBox1);
+            ClientSize = new Size(814, 540);
+            Controls.Add(showMessageLayoutPanel);
+            Controls.Add(panel1);
             Icon = (Icon)resources.GetObject("$this.Icon");
             MaximizeBox = false;
             Name = "ChatForm";
             SizeGripStyle = SizeGripStyle.Hide;
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Synapse";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private ListBox listBox1;
         private TextBox txtMessage;
         private Button btnSend;
         private Button btnSendFile;
+        private FlowLayoutPanel showMessageLayoutPanel;
+        private Panel panel1;
     }
 }

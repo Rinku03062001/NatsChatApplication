31922dc9178fe1aa86908e67722d0cdd64daafe7
Tapobanta <tapobanta43@gmail.com>
2025-09-11 19:59:46 +0530

    Describes the properties of ChatMessage

---
+++ Models/ChatMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatAppNats.Models
+{
+    public class ChatMessage
+    {
+        public string? Type { get; set; } // "message" or "status"
+        public string? FileName { get; set; } // Optional, for file messages
+        public byte[] Data { get; set; } = Array.Empty<byte>();// Optional, for file messages
+        public DateTime TimeStamp { get; set; } = DateTime.UtcNow;
+        public string UserName { get; set; } = string.Empty;
+    }
+}

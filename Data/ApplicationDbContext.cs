f6b208d22ac99c7508e71d337d6b109afc20053b
Tapobanta <tapobanta43@gmail.com>
2025-09-20 19:28:08 +0530

    Add Data folder for creating database in SQL Server using EF Core

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ChatAppNats.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatAppNats.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> users {  get; set; }
+        public DbSet<Models.Message> Messages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=RINKU-LAPPY\SQLEXPRESS; Database=ChatAppDB; TrustServerCertificate=True; Trusted_Connection=True");
+        }
+    }
+}

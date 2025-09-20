ebddd96dfbe3fd11f77ee4b72f269a7dfc731da4
Tapobanta <tapobanta43@gmail.com>
2025-09-20 19:34:41 +0530

    Models folder contains all the necessary class that are created to store the user and message details

---
+++ Models/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatAppNats.Models
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public int SenderId { get; set; }
+        public int ReceiverId { get; set; }
+        public string? Text { get; set; }
+        public DateTime SendAt { get; set; } = DateTime.Now;
+        public bool IsRead { get; set; } = false;
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatAppNats.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; } // PK
+        [Required]
+        public string? UserName { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public string? PasswordHash { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}

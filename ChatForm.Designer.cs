359a06945775230b6a936359b14dfdb55aec9e00
Tapobanta <tapobanta43@gmail.com>
2025-09-20 19:36:47 +0530

    improve the UI by adding the Split Panel, the left panel shows all the users and the right panel is used to show messages.

--- ChatForm.Designer.cs
+++ ChatForm.Designer.cs
@@ -34,22 +34,28 @@
             btnSendFile = new Button();
             panel1 = new Panel();
             lstLogs = new ListBox();
+            splitContainer1 = new SplitContainer();
+            flowLayoutPanelUsers = new FlowLayoutPanel();
             panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
             SuspendLayout();
             // 
             // txtMessage
             // 
-            txtMessage.Location = new Point(121, 11);
+            txtMessage.Location = new Point(106, 15);
             txtMessage.Multiline = true;
             txtMessage.Name = "txtMessage";
-            txtMessage.Size = new Size(600, 67);
+            txtMessage.Size = new Size(597, 67);
             txtMessage.TabIndex = 1;
             // 
             // btnSend
             // 
             btnSend.BackColor = Color.MediumTurquoise;
             btnSend.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnSend.Location = new Point(718, 11);
+            btnSend.Location = new Point(706, 14);
             btnSend.Name = "btnSend";
             btnSend.Size = new Size(88, 67);
             btnSend.TabIndex = 2;
@@ -61,7 +67,7 @@
             // 
             btnSendFile.BackColor = Color.Turquoise;
             btnSendFile.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnSendFile.Location = new Point(9, 13);
+            btnSendFile.Location = new Point(3, 15);
             btnSendFile.Name = "btnSendFile";
             btnSendFile.Size = new Size(106, 65);
             btnSendFile.TabIndex = 3;
@@ -70,12 +76,12 @@
             // 
             // panel1
             // 
+            panel1.Controls.Add(txtMessage);
             panel1.Controls.Add(btnSendFile);
             panel1.Controls.Add(btnSend);
-            panel1.Controls.Add(txtMessage);
-            panel1.Location = new Point(3, 359);
+            panel1.Location = new Point(3, 466);
             panel1.Name = "panel1";
-            panel1.Size = new Size(809, 86);
+            panel1.Size = new Size(797, 87);
             panel1.TabIndex = 4;
             // 
             // lstLogs
@@ -87,8 +93,33 @@
             lstLogs.ItemHeight = 25;
             lstLogs.Location = new Point(3, 6);
             lstLogs.Name = "lstLogs";
-            lstLogs.Size = new Size(809, 354);
+            lstLogs.Size = new Size(794, 454);
             lstLogs.TabIndex = 5;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Location = new Point(3, 6);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(flowLayoutPanelUsers);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(panel1);
+            splitContainer1.Panel2.Controls.Add(lstLogs);
+            splitContainer1.Size = new Size(1056, 553);
+            splitContainer1.SplitterDistance = 249;
+            splitContainer1.TabIndex = 6;
+            // 
+            // flowLayoutPanelUsers
+            // 
+            flowLayoutPanelUsers.AutoScroll = true;
+            flowLayoutPanelUsers.Location = new Point(3, 3);
+            flowLayoutPanelUsers.Name = "flowLayoutPanelUsers";
+            flowLayoutPanelUsers.Size = new Size(243, 543);
+            flowLayoutPanelUsers.TabIndex = 0;
             // 
             // ChatForm
             // 
@@ -96,9 +127,8 @@
             AutoScaleMode = AutoScaleMode.Font;
             AutoSize = true;
             BackColor = Color.MistyRose;
-            ClientSize = new Size(814, 451);
-            Controls.Add(lstLogs);
-            Controls.Add(panel1);
+            ClientSize = new Size(1058, 565);
+            Controls.Add(splitContainer1);
             Icon = (Icon)resources.GetObject("$this.Icon");
             MaximizeBox = false;
             Name = "ChatForm";
@@ -107,6 +137,10 @@
             Text = "Synapse";
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -116,5 +150,7 @@
         private Button btnSendFile;
         private Panel panel1;
         private ListBox lstLogs;
+        private SplitContainer splitContainer1;
+        private FlowLayoutPanel flowLayoutPanelUsers;
     }
 }

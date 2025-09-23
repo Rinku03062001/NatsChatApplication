b7adb2634fd0936f223cc30bc963ad2514be108b
Tapobanta <tapobanta43@gmail.com>
2025-09-23 18:50:26 +0530

    Add 2 class - Group.cs and GroupMember.cs

---
+++ Models/Group.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatAppNats.Models
+{
+    public class Group
+    {
+        [Key]
+        [Required]
+        public int GroupId { get; set; }
+
+        [Required]
+        public string GroupName { get; set; } = string.Empty;
+
+        [Required]
+        public string CreatedBy { get; set; } = string.Empty;
+
+        public DateTime CreateDate { get; set; } = DateTime.UtcNow; 
+
+    }
+}
---
+++ Models/GroupMember.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatAppNats.Models
+{
+    public  class GroupMember
+    {
+        [Key]
+        [Required]
+        public int GroupMemberId { get; set; }
+
+        [Required]
+        [ForeignKey("Groups")]
+        public int GroupId { get; set; }
+
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+
+
+        public DateTime AddedAt = DateTime.UtcNow;
+
+       
+        public Group Groups { get; set; } = new Group();
+
+
+    }
+}

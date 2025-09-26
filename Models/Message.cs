55e04c7e4f55cfc5f982b06741fcce475b7f1ed7
Tapobanta <tapobanta43@gmail.com>
2025-09-26 21:52:50 +0530

    Add GroupId inside the Message class

--- Models/Message.cs
+++ Models/Message.cs
@@ -10,7 +10,13 @@ namespace ChatAppNats.Models
     {
         public int MessageId { get; set; }
         public int SenderId { get; set; }
+
+        // for 1-to-1 chats
         public int ReceiverId { get; set; }
+
+        // for group chats
+        public int? GroupId { get; set; }
+
         public string? Text { get; set; }
         public DateTime SendAt { get; set; } = DateTime.Now;
         public bool IsRead { get; set; } = false;

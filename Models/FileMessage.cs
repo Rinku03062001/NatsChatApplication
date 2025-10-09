041592f7d5a0d22d6300767528d6c7f5c2ab50a2
Tapobanta <tapobanta43@gmail.com>
2025-10-09 21:12:41 +0530

    Add FileMessage table

---
+++ Models/FileMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatAppNats.Models
+{
+    public class FileMessage
+    {
+        [Key]
+        public int FileId { get; set; }
+        public string Sender { get; set; }
+        public string Receiver { get; set; } // Can be a user or a group
+        public int? GroupId { get; set; }
+        public string FileName { get; set; }
+        public string FileData { get; set; } // Base64 encoded string
+        public string MessageType { get; set; } = "file"; // To differentiate between text and file messages
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    }
+}
